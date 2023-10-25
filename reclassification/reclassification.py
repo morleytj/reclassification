958f6a0b4e952c15967ec3e2c7fd7c896a84cb86
Theodore Morley <theodore.j.morley@vumc.org>
2023-10-25 18:21:17 -0400

    Moved file into directory for packaging

--- reclassification/reclassification.py
+++ reclassification/reclassification.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-'''
-Input: Series with new probabilities, series with old probabilities, series with outcome, cutoff for NRI calculation.
-Output: NRI
-Note: If cutoff is set to zero, this constitutes "continuous NRI".
-'''
 def calculate_nri(prob_new, prob_old, outcome, cutoff):
+    '''
+    Input: Series with new probabilities, series with old probabilities, series with outcome, cutoff for NRI calculation.
+    Output: NRI
+    Note: If cutoff is set to zero, this constitutes "continuous NRI".
+    '''
     t_df = pd.DataFrame()
     t_df['new_prob'] = prob_new
     t_df['old_prob'] = prob_old
@@ -18,11 +18,11 @@ def calculate_nri(prob_new, prob_old, outcome, cutoff):
     nri = (t_df.loc[(t_df.outcome==1)&(t_df.updown==1)].shape[0]/casenum-t_df.loc[(t_df.outcome==1)&(t_df.updown==-1)].shape[0]/casenum)+((t_df.loc[(t_df.outcome==0)&(t_df.updown==-1)].shape[0]/controlnum-t_df.loc[(t_df.outcome==0)&(t_df.updown==1)].shape[0]/controlnum))
     return nri
 
-'''
-Input: Dataframe containing outcome (named colname_cc), new probabilities (colname_prob_new), and old probabilities (named colname_prob_old), and those column names.
-Output: IDI
-'''
 def calculate_idi_colname(probs, colname_cc, colname_prob_new, colname_prob_old):
+    '''
+    Input: Dataframe containing outcome (named colname_cc), new probabilities (colname_prob_new), and old probabilities (named colname_prob_old), and those column names.
+    Output: IDI
+    ''' 
     meanCaseNew = probs.loc[probs[colname_cc]==1, colname_prob_new].mean()
     meanControlNew = probs.loc[probs[colname_cc]==0, colname_prob_new].mean()
     meanCaseOld = probs.loc[probs[colname_cc]==1, colname_prob_old].mean()
@@ -30,11 +30,11 @@ def calculate_idi_colname(probs, colname_cc, colname_prob_new, colname_prob_old)
     idi = (meanCaseNew-meanControlNew)-(meanCaseOld-meanControlOld)
     return idi
 
-'''
-Input: Series containing case_status, series containing new probabilities, series containing old probabilities.
-Output: IDI
-'''
 def calculate_idi(case_status, new_probabilities, old_probabilities):
+    '''
+    Input: Series containing case_status, series containing new probabilities, series containing old probabilities.
+    Output: IDI
+    '''
     #set up working dataframe
     t_df = pd.DataFrame()
     t_df['new_prob']=new_probabilities
